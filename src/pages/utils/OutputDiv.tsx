29bd351df30a4e36a9c9900be8c827040375dfd2
isk-bokang <bo.kang@iskra.world>
2022-07-14 18:29:25 +0900

    feat: adjust single view div

--- src/pages/utils/OutputDiv.tsx
+++ src/pages/utils/OutputDiv.tsx
@@ -38,26 +38,28 @@ export function TargListView(prop: listProps) {
 type targProps = { targ: any }
 
 export function TargView(prop: targProps) {
-    const [keys, setKeys] = useState<string[]>([])
+    const [columns, setColumns] = useState<ColumnsType<any[]>>([])
+    const [data, setData] = useState<any>()
 
     useEffect(() => {
-        let tmpKeys: string[] = []
-        tmpKeys = tmpKeys.concat(Object.keys(prop.targ))
-        setKeys(tmpKeys)
+        prop.targ.key = prop.targ.id
+        setData( [prop.targ] )
+        setColumns(
+            Object.keys(prop.targ).map(item => {
+                return {
+                    title: item,
+                    dataIndex: item,
+                    key: item
+                }
+            }).filter(item =>{
+                return item.title !== 'key'
+            })
+        )
     }, [prop.targ])
 
     return (
         <div>
-            <table border={1} style={{ alignItems: "left" }}>{
-                keys.map(key => {
-                    return (
-                        <tr>
-                            <th>{key}</th>
-                            <td align={"left"}>{prop.targ[key]}</td>
-                        </tr>
-                    )
-                })}
-            </table>
+            <Table columns={columns} dataSource={data}  />
         </div>
     )
 
