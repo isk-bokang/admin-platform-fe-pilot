25e47eb7d21939149069b35854814f9cb5be2ed9
isk-bokang <bo.kang@iskra.world>
2022-08-23 17:42:40 +0900

    feat: add role modify component (yet)

--- src/pages/wallet/WalletsDiv.tsx
+++ src/pages/wallet/WalletsDiv.tsx
@@ -1,14 +1,37 @@
-import {Table} from 'antd';
+import {Form, Input, Table} from 'antd';
 import type {ColumnsType} from 'antd/es/table';
-import React from 'react';
+import React, {useState} from 'react';
 import {PlatformWalletInfo} from "../apis/WalletApi";
 
-export function WalletListDiv() {
+const NONE = -1
 
-    const dummyData: PlatformWalletInfo[] = [{
+const dummyData: PlatformWalletInfo[] = [{
+    name: 'asd',
+    id: 1,
+    walletAddress: 'wallet address1',
+    walletContractInfoList: [
+        {
+            contractId: 1,
+            contractAddress: "contract1",
+            contractType: "contract1",
+            contractName: "contract1",
+            role: "qweqwe",
+            roleId: 1
+        },
+        {
+            contractId: 2,
+            contractAddress: "contract1",
+            contractType: "contract1",
+            contractName: "contract1",
+            role: "qweqwe",
+            roleId: 2
+        }
+    ]
+},
+    {
         name: 'asd',
-        id: 1,
-        walletAddress: 'wallet address1',
+        id: 2,
+        walletAddress: 'wallet address2',
         walletContractInfoList: [
             {
                 contractId: 1,
@@ -16,65 +39,59 @@ export function WalletListDiv() {
                 contractType: "contract1",
                 contractName: "contract1",
                 role: "qweqwe",
-                roleId: 1
+                roleId: 3
             },
             {
                 contractId: 2,
-                contractAddress: "contract1",
-                contractType: "contract1",
-                contractName: "contract1",
+                contractAddress: "contract2",
+                contractType: "contract2",
+                contractName: "contract2",
                 role: "qweqwe",
-                roleId: 2
+                roleId: 4
             }
         ]
-    },
-        {
-            name: 'asd',
-            id: 2,
-            walletAddress: 'wallet address2',
-            walletContractInfoList: [
-                {
-                    contractId: 1,
-                    contractAddress: "contract1",
-                    contractType: "contract1",
-                    contractName: "contract1",
-                    role: "qweqwe",
-                    roleId: 3
-                },
-                {
-                    contractId: 2,
-                    contractAddress: "contract2",
-                    contractType: "contract2",
-                    contractName: "contract2",
-                    role: "qweqwe",
-                    roleId: 4
-                }
-            ]
-        }]
+    }]
 
-    interface DataType {
-        key ?: number;
-        id ?: number,
-        name ?: string,
-        walletAddress : string,
-        contractAddress ?: string,
-        contractName ?: string,
-        role ?: string,
-        contractType ?: string,
-        spanCnt ?: number
+export function WalletListDiv() {
+
+    const [form] = Form.useForm();
+    const [data, setData] = useState<PlatformWalletInfo[]>(dummyData);
+    const [editingKey, setEditingKey] = useState<number>(NONE);
+
+    const isEditing = (record: AttributeType) => record.key === editingKey;
+
+    interface AttributeType {
+        key?: number;
+        id?: number,
+        name?: string,
+        walletAddress?: string,
+        contractAddress?: string,
+        contractName?: string,
+        role?: string,
+        contractType?: string,
+        spanCnt?: number
     }
 
-    function generateData(originData : PlatformWalletInfo[]){
-        let ret : DataType[] = []
-        for(const item of originData){
-            let commonData : DataType ={
-                id : item.id,
-                name : item.name,
-                walletAddress : item.walletAddress,
-                spanCnt : item.walletContractInfoList.length
+    function edit( record: Partial<AttributeType> & { key: React.Key }) {
+        form.setFieldsValue({ walletAddress : '', contractAddress : '', contractName : '', role : '', ...record });
+        setEditingKey(record.key);
+    }
+
+    function cancel () {
+        setEditingKey(NONE);
+    };
+
+    function generateData() {
+        let ret: AttributeType[] = []
+        for (const item of data) {
+            let commonData: AttributeType = {
+                id: item.id,
+                name: item.name,
+                walletAddress: item.walletAddress,
+                spanCnt: item.walletContractInfoList.length
             }
-            for(const jtem of item.walletContractInfoList){
-                let retItem : DataType = JSON.parse(JSON.stringify(commonData))
+            for (const jtem of item.walletContractInfoList) {
+                let retItem: AttributeType = JSON.parse(JSON.stringify(commonData))
                 retItem.key = jtem.roleId
                 retItem.contractAddress = jtem.contractAddress
                 retItem.contractName = jtem.contractName
@@ -84,60 +101,101 @@ export function WalletListDiv() {
                 commonData.spanCnt = 0
             }
         }
-
-
         return ret
     }
 
-    function onCellHandle(record){
-        return {rowSpan : record.spanCnt}
+    function onCellHandle(record) {
+        return {rowSpan: record.spanCnt}
     }
 
-    const columns: ColumnsType<DataType> = [
+    interface EditableCellProps extends React.HTMLAttributes<HTMLElement> {
+        editing: boolean;
+        dataIndex: string;
+        title: string;
+        record: AttributeType;
+        index: number;
+        children: React.ReactNode;
+        key: number
+    }
+
+    function EditableCell(prop: EditableCellProps) {
+
+        const inputNode =  <Input />;
+
+        return (
+            <td {...prop}>
+                {prop.editing ? (
+                    <Form.Item
+                        name={prop.dataIndex}
+                        style={{margin: 0}}
+                        rules={[
+                            {
+                                required: true,
+                                message: `Please Input ${prop.title}!`,
+                            },
+                        ]}
+                    >
+                        {inputNode}
+                    </Form.Item>
+                ) : (
+                    prop.children
+                )}
+            </td>
+        );
+    }
+
+    const columns = [
         {
             key: 1,
             title: "ID",
             dataIndex: 'id',
-            onCell : onCellHandle
+            editable : false,
+            onCell: onCellHandle
         },
         {
             key: 2,
             title: "Wallet Name",
             dataIndex: 'name',
-            onCell : onCellHandle
+            editable : false,
+            onCell: onCellHandle
         },
         {
             key: 3,
             title: "Wallet Address",
             dataIndex: 'walletAddress',
-            onCell : onCellHandle
+            editable : false,
+            onCell: onCellHandle
         },
         {
             key: 4,
             title: "Contract Address",
             dataIndex: 'contractAddress',
+            editable : true,
         },
         {
             key: 5,
             title: "Contract Name",
             dataIndex: 'contractName',
+            editable : true,
         },
         {
             key: 6,
             title: "Contract Type",
             dataIndex: 'contractType',
+            editable : true,
 
         },
         {
             key: 7,
             title: "Role",
             dataIndex: 'role',
+            editable : true,
         },
     ]
 
     return (
         <div>
-            <Table columns={columns} bordered dataSource={generateData(dummyData)}/>
+            <Table columns={columns} bordered dataSource={generateData()}/>
         </div>
     )
 }
